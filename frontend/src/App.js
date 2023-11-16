dc8714bf53dcf4a0d1ab99d5aeeef4da1cef2e05
Vinko <vinkorakic@oakniiv.com>
2023-11-16 14:47:02 +0100

    push

---
+++ frontend/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(null); 
+
+  const responseGoogle = (response) => {
+    if (response.profileObj) {
+      setUser({
+        email: response.profileObj.email,
+      });
+    }
+  };
+
+  function toggleTor(torNummer) {
+    const gateName = `open${torNummer}`; //war früher value in der Form
+    const bodyData = {
+      secret: "ultra-geheim", //TODO?
+      gate: gateName,
+      userEmail: user ? user.email : null, 
+    };
+
+    fetch('http://127.0.0.1:8080/api/toggle', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then(function (response) {
+          if (!response.ok) {
+              throw new Error('error');
+          }
+      })
+      .catch(function (error) {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <div className="container">
+      {user ? (
+        <div>Hallo, {user.email}</div>
+      ) : (
+        <GoogleLogin
+          clientId="client id????" 
+          buttonText="Google Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true} // isSignedIn={true} attribute will call onSuccess callback on load to keep the user signed in.
+        />
+        // https://www.npmjs.com/package/react-google-login
+      )}
+      <br></br>
+      <br></br>
+      <img
+        className="logo"
+        src="https://www.b-ite.de/assets/images/bite-logo-9606eea416a94915.svg"
+        alt="BITE GmbH Logo"
+      />
+      <div className="button-container">
+        {[1, 2, 3, 4].map((torNummer) => (
+          <button
+            key={torNummer}
+            className="neu-button"
+            onClick={() => toggleTor(torNummer)}
+          >
+            <strong>Tor {torNummer}</strong>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 var secretCheckKey = "ultra-geheim"
@@ -15,27 +17,35 @@ var gateGpioMap = make(map[string]int)
 type jsonBody struct {
 	Secret string `json:"secret"`
 	Gate   string `json:"gate"`
+	Email  string `json:"userEmail"` // kommt vom Frontend json, nur überprüfen ob mail string mit b-ite endet oder so
 }
 
 func toggleGPIO(pin int) error {
-	cmd := exec.Command("gpio", "toggle", fmt.Sprintf("%d", pin)) //https://www.geeksforgeeks.org/fmt-sprintf-function-in-golang-with-examples/
+	cmd := exec.Command("gpio", "toggle", fmt.Sprintf("%d", pin))
 	return cmd.Run()
 }
 
 func toggleGate(gate string) {
+	fmt.Print("TOGGLE GATE ENTER")
 	gpio, ok := gateGpioMap[gate]
 
 	if !ok {
+		fmt.Print("NOT OK")
 		return
 	}
 
 	toggleGPIO(gpio)
 	time.Sleep(time.Second * 1)
 	toggleGPIO(gpio)
+	fmt.Print("TOGGLEGATE EXIT")
 }
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 	e.HideBanner = true
 
 	gateGpioMap["open1"] = 22
@@ -48,24 +58,36 @@ func main() {
 	})
 
 	e.POST("/api/toggle", func(c echo.Context) error {
+		fmt.Print("SANITY CHECK 1")
 		var payload = new(jsonBody)
 
 		payloadBindError := c.Bind(payload)
 
 		if payloadBindError != nil {
+			fmt.Print("payloadBindError")
 			return echo.NewHTTPError(http.StatusBadRequest, payloadBindError)
 		}
 
 		if payload.Secret != secretCheckKey {
+			fmt.Print("INSIDE SECRET")
 			return c.NoContent(http.StatusForbidden)
 		}
 
-		if payload.Gate == "" {
-			return c.NoContent(http.StatusBadRequest)
+		if !strings.HasSuffix(payload.Email, "@b-ite.de") { // bite.net auch?
+			return c.NoContent(http.StatusUnauthorized)
 		}
 
-		go toggleGate(payload.Gate)
+		if !strings.HasSuffix(payload.Email, "@b-ite.com") {
+			return c.NoContent(http.StatusUnauthorized)
+		}
 
+		if payload.Gate == "" {
+			fmt.Print("payload gate empty")
+			return c.NoContent(http.StatusBadRequest)
+		}
+		fmt.Print("SANITY CHECK 2")
+		go toggleGate(payload.Gate)
+		fmt.Print("SANITY CHECK 3")
 		return c.NoContent(http.StatusOK)
 	})
 
