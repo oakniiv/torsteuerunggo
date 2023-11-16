bc83376029ca462af4bf4eb4d46fffe9a8e8c330
Vinko Rakić <vinkorakic@oakniiv.com>
2023-11-16 09:53:17 +0100

    LET'SGOOOOOOOO

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/warthog618/gpiod"
+	"fmt"
+	"net/http"
+
+	//"github.com/warthog618/gpiod"
+	"os/exec"
 )
 
 // PIN_CH1     = 25
@@ -11,51 +13,24 @@ import (
 // PIN_CH3     = 23
 // PIN_CH4     = 22
 
-func toggleGPIO() {
-    chip, err := gpiod.NewChip("gpiochip0")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer chip.Close()
-
-    line, err := chip.RequestLine(25, gpiod.AsOutput(0)) // PIN_CH4     = 22
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer line.Close()
-
-    val, err := line.Value()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
-    if val == 0 {
-        line.SetValue(1)
-        fmt.Println("SANITY CHECK 1")
-    } else {
-        line.SetValue(0)
-        fmt.Println("SANITY CHECK 2")
-    }
+func toggleGPIO(pin int) error {
+	cmd := exec.Command("gpio", "toggle", fmt.Sprintf("%d", pin))
+	return cmd.Run()
 }
 
 func buttonHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        toggleGPIO()
+	if r.Method == "POST" {
+		toggleGPIO(25)
 		fmt.Fprintf(w, `<html><body><form action="/" method="post"><input type="submit" value="Toggle GPIO"></form></body></html>`)
-    } else {
-        fmt.Fprintf(w, `<html><body><form action="/" method="post"><input type="submit" value="Toggle GPIO"></form></body></html>`)
-    }
+	} else {
+		fmt.Fprintf(w, `<html><body><form action="/" method="post"><input type="submit" value="Toggle GPIO"></form></body></html>`)
+	}
 }
 
-
 func main() {
-
-    http.HandleFunc("/", buttonHandler)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println(err.Error())
-    }
+	http.HandleFunc("/", buttonHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
